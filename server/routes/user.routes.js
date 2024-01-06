b6b373e7df5931653f135ede93c5045020372533
himanshu1161 <mishrah.11603@gmail.com>
2024-01-06 12:06:29 +0530

    initial commit

---
+++ server/routes/user.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import{
+    createUser, getAllusers, getUserInfoByID
+} from '../controllers/user.controller.js'
+
+const router = express.Router();
+
+router.route('/').get(getAllusers);
+router.route('/').post(createUser);
+router.route('/:id').get(getUserInfoByID);
+
+export default router;
